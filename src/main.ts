bbbb9ce9da6b25964b86b6077bf6ba8e0ea665fd
Nick Lowman <digital@nicklowman.co.uk>
2016-02-23 16:19:13 +0000

    Got bootstrap working with webpack

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-
 import {SeedApp} from './app/app';
+import {LocationStrategy, HashLocationStrategy} from "angular2/router";
+import "style!css!bootstrap/dist/css/bootstrap.css";
 
-bootstrap(SeedApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
-  .catch(err => console.error(err));
+bootstrap(SeedApp, [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
+]).catch(err => console.error(err));
--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,12 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.ts$/,  loader: 'ts-loader' },
-      { test: /\.css$/, loader: 'style!css' }
+      { test: /\.ts$/,  loader: 'ts-loader'},
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ],
     noParse: [ path.join(__dirname, 'node_modules', 'angular2', 'bundles') ]
   },
